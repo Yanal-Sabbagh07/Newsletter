d1dc353f20387b8a502b3dfbde44150edfbfb50c
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2022-07-24 13:56:54 +0200

    setting up the server to listen on port 3000

---
+++ index.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.get("/", function (req, res) {
+  res.send("Welcome to my Newsletter");
+});
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
+});
