47c44075c7766f89ee78ea200572d0c4ba31f27d
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2022-08-28 19:47:24 +0200

    setting up the server and get user details

--- index.js
+++ index.js
@@ -3,10 +3,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
+app.use(express.static("public")); // Allow to use images and css files
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
-  res.send("Welcome to my Newsletter");
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 });
 app.listen(3000, function () {
   console.log("server is running on port 3000");
