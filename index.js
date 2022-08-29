501f02c0b80b48bfd8ea73ffc9dd070cf00a9841
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2022-08-29 11:59:27 +0200

    add success and failure pages

--- index.js
+++ index.js
@@ -35,10 +35,16 @@ app.post("/", function (req, res) {
 
   const options = {
     method: "POST",
-    auth: "yano:ed28b90b7912f5d3a8c085c193773dea-us17",
+    auth: "yano:1ed28b90b7912f5d3a8c085c193773dea-us17",
   };
 
   const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
     response.on("data", function (data) {
       console.log(JSON.parse(data));
     });
@@ -46,6 +52,14 @@ app.post("/", function (req, res) {
 
   request.write(jsonData);
   request.end();
+});
+
+app.post("/success", function (req, res) {
+  res.redirect("/");
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 });
 app.listen(3000, function () {
   console.log("server is running on port 3000");
